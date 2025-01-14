e0ed98fe0ddc092cf4d189ec5f3ae1a4704e63bc
BrianChuquiruna <bryan_virgo_6@hotmail.com>
2025-01-14 15:56:41 -0500

    Nuevo renglon agregado al proyecto

--- obj/Debug/net8.0/PlatziWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlatziWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatziWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69937245fb20bea4716a376d7acff3f46a78f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatziWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatziWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
